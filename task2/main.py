f557fd6e9e9da75853c13b7048618b2e4bbad442
kachuros <Stanislav.Kachurovskyi@partenaires.videotron.com>
2024-07-08 14:38:34 +0300

    fixed task2

--- task2/main.py
+++ task2/main.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 def is_palindrome(s: str) -> bool:
-    s = s.lower()
+    s = ''.join(s.lower().split())
     
     dq = deque(s)
     
@@ -12,10 +12,10 @@ def is_palindrome(s: str) -> bool:
     return True
 
 def main():
-    print(is_palindrome("aa"))
-    print(is_palindrome("a b a"))
-    print(is_palindrome("hello"))
-    print(is_palindrome("tenet"))
+    print(is_palindrome("aa  "))
+    print(is_palindrome("a  ba"))
+    print(is_palindrome("hello "))
+    print(is_palindrome("ten et"))
     print(is_palindrome("test"))
 
 if __name__ == "__main__":
